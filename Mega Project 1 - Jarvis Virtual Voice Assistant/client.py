0a891c7ed5c64969555d9e9d8570fefcd774585c
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-07-03 12:32:30 +0530

    In Progress

--- Mega Project 1 - Jarvis Virtual Voice Assistant/client.py
+++ Mega Project 1 - Jarvis Virtual Voice Assistant/client.py
@@ -1,17 +1,21 @@
-from openai import OpenAI
+# from openai import OpenAI
 
-client = OpenAI(
-    api_key="",
-)
+# client = OpenAI(
+#     api_key="",
+# )
 
-completion = client.chat.completions.create(
-    model="gpt-4.1",
-    messages=[
-        {
-            "role": "user",
-            "content": "Write a one-sentence bedtime story about a unicorn."
-        }
-    ]
-)
+# completion = client.chat.completions.create(
+#     model="gpt-4.1",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "You are a virtual assistant named jarvis skilled in general tasks like alexa and google cloud"
+#         },
+#         {
+#             "role": "user",
+#             "content": "What is coding"
+#         }
+#     ]
+# )
 
-print(completion.choices[0].message.content)
+# print(completion.choices[0].message.content)
